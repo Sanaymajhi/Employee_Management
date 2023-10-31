e9cc7ab8d7e135726ee499ea5a094cbd5a885397
GopsJadhao <gjadhao1496@gmail.com>
2023-10-31 22:23:21 +0530

    Revert "EmployeeManagement Task"

--- src/main/java/com/EmployeeManagement/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.EmployeeManagement.repository;
-
-import com.EmployeeManagement.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
